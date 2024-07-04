abdc94595d18825a4f8e30a7be8e291f34b374bd
savelyevresearch <savelyevresearch@protonmail.com>
2024-07-04 22:15:43 +0600

    Issues with imports for the UserModule are solved

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { BullModule } from "@nestjs/bull";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    BullModule.registerQueue({ name: "user-status" }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
