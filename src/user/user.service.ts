06fc179bb2a4ce74a81dcbe370cb6fd59b67d11e
savelyevresearch <savelyevresearch@protonmail.com>
2024-07-04 17:03:12 +0600

    Method for getting a user with Redis caching is added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,19 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private usersRepository: Repository<User>, @InjectQueue("user-status") private userStatusQueue: Queue) {}
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectQueue("user-status") private userStatusQueue: Queue,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const existingUser = await this.usersRepository.findOne({ where: { email: createUserDto.email } });
@@ -22,6 +27,18 @@ export class UserService {
     await this.usersRepository.save(user);
 
     await this.userStatusQueue.add('update-status', { userId: user.id }, { delay: 10000 });
+
+    return user;
+  }
+
+  async get(id: number) {
+    const user = await this.usersRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new BadRequestException("ERR_USER_NOT_FOUND");
+    }
+
+    await this.cacheManager.set(`user-${id}`, user, 1800);
 
     return user;
   }
