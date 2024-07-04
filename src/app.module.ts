1cd44fc463108efd8478a36d6daf9c4ece20f9f7
savelyevresearch <savelyevresearch@protonmail.com>
2024-07-04 15:43:02 +0600

    Database module is removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,32 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { BullModule } from "@nestjs/bull";
-import { DatabaseModule } from "./database/database.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DATABASE_HOST"),
+        port: Number(configService.get<number>("DATABASE_PORT")),
+        username: configService.get<string>("DATABASE_USER"),
+        password: configService.get<string>("DATABASE_PASSWORD"),
+        database: configService.get<string>("DATABASE_NAME"),
+        entities: [__dirname + "/../**/*/.entity{.ts,.js}"],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
     BullModule.forRoot({
       redis: {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
       },
     }),
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
