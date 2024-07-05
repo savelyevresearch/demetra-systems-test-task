5a7405ba4cc512273754c2da83bec3343018f50f
savelyevresearch <savelyevresearch@protonmail.com>
2024-07-05 13:36:33 +0600

    Corresponding UNIT tests are completed

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,28 +1,57 @@
 // src/users/users.service.spec.ts
 import { Test, TestingModule } from "@nestjs/testing";
-import { getRepositoryToken } from "@nestjs/typeorm";
+import { TypeOrmModule, getRepositoryToken } from "@nestjs/typeorm";
 import { UserService } from "./user.service";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { UserDto } from "./dto/user.dto";
+import { BullModule, getQueueToken } from "@nestjs/bull";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import Bull, { Queue } from "bull";
 
 describe("UsersService", () => {
   let service: UserService;
   let repository: Repository<User>;
+  let cacheManager: any;
+  let bullQueue: Queue;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [BullModule.registerQueue({ name: "user-status" })],
       providers: [
         UserService,
         {
           provide: getRepositoryToken(User),
-          useClass: Repository,
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+            create: jest.fn().mockImplementation((user) => user),
+          },
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+        {
+          provide: getQueueToken("user-status"),
+          useValue: {
+            add: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<UserService>(UserService);
     repository = module.get<Repository<User>>(getRepositoryToken(User));
+    cacheManager = module.get(CACHE_MANAGER);
+    bullQueue = module.get(getQueueToken("user-status"));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("should be defined", () => {
